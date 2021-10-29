e800a4d8ad6ac87da7647d5f9ea023db98041733
Peder0210 <55102034+Peder0210@users.noreply.github.com>
2021-10-29 10:05:22 +0200

    Update App.js
    TIlføjet firebase

--- App.js
+++ App.js
@@ -2,6 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBrjHmQpi_Lu44JprUakJ5fttJ9P3c5NCo",
+  authDomain: "opgave3-80853.firebaseapp.com",
+  databaseURL: "https://opgave3-80853-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "opgave3-80853",
+  storageBucket: "opgave3-80853.appspot.com",
+  messagingSenderId: "850548815144",
+  appId: "1:850548815144:web:e3b3c9ccf09997e7424c1d"
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
